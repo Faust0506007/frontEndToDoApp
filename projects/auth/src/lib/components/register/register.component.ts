9168ae2e97931ebdab5de5e841893b3166676961
Th220284 <mth0506007@gmail.com>
2023-02-19 12:21:17 +1100

    updateFrontEnd

--- projects/auth/src/lib/components/register/register.component.ts
+++ projects/auth/src/lib/components/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+
+
 
 @Component({
   selector: 'lib-register',
@@ -7,8 +13,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  hide= true;
-  
+  hide= false;
+  returnUrl: string;
+  error: string;
+
   form: FormGroup = new FormGroup(
     {
       email: new FormControl(null, [Validators.required, Validators.email]),
@@ -18,7 +26,11 @@ export class RegisterComponent {
     }
   );
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   get email(): FormControl {
     return this.form.get('email') as FormControl;
@@ -35,4 +47,36 @@ export class RegisterComponent {
   get passwordConfirm(): FormControl {
     return this.form.get('passwordConfirm') as FormControl;
   }
+  get f() {
+    return this.form.controls;
+  }
+
+  ngOnInit() {
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
+  }
+
+  onSubmit() {
+    this.hide = true;
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.authService
+      .register(this.f.username.value, this.f.password.value, this.f.email.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.error = '';
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.error = error;
+        }
+      );
+  }
+
 }
--- projects/auth/src/lib/services/auth.service.ts
+++ projects/auth/src/lib/services/auth.service.ts
@@ -49,6 +49,22 @@ export class AuthService {
 		);
 	}
 
+  register(username: string, password: string, email: string) {
+		return this.http.post<any>('/auth/register', { username, password, email }).pipe(
+			map(user => {
+				// login successful if there's a jwt token in the response
+				if (user && user.accessToken) {
+					// store; user; details; and; jwt; token in local
+					// storage; to; keep; user; logged in between; page; refreshes;
+
+					localStorage.setItem('currentUser', JSON.stringify(user));
+					this.currentUserSubject.next(user);
+				}
+				return user;
+			})
+		);
+	}
+
 	logout() {
 		// remove user from local storage to log user out
 		localStorage.removeItem('currentUser');
